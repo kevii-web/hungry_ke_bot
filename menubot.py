7e8dbaccf12ccfb7843768e8018b8c08e46f4366
yuheong <yuhe_ong@hotmail.com>
2019-02-01 02:14:02 +0800

    Remove code that prints debug info

--- menubot.py
+++ menubot.py
@@ -17,7 +17,6 @@ logger = logging.getLogger(__name__)
 def start(bot, update):
     """Send the menu when the command /start is issued."""
     text = firebaselink.getMenu()
-    print(text)
     print(str(update.message.from_user));
     bot.send_message(chat_id=update.message.chat_id, text=text, parse_mode="markdown")
 
